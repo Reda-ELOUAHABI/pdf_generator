76b851b4697f699b0eb99f15d97adfa4014f9651
reda-coder <redaelouahabi98@gmail.com>
2021-10-02 03:13:34 +0100

    blessed step hamdollah

--- script.js
+++ script.js
@@ -200,7 +200,14 @@ function quantityChanged(
         console.log(id);
         dataProduit.forEach(produit => {
             if(produit.id==id){
-                dataProduit4PDF.push(produit);
+                dataProduit4PDF.push({
+                    
+                    "prix": prix,
+                    "title": produit.title,
+                    "quanity": value,
+                    "totalPrix" : prix*value
+
+                });
             console.log(produit);
             }
             
@@ -517,7 +524,8 @@ function myFunction2() {
 
 
     //  FIll Products Rows
-    GetSelected();
+    // it was causing a problem on data4PDF.shift()
+    // GetSelected();
 
 
 
@@ -586,7 +594,7 @@ console.log("let see products ");
 
         var select = document.getElementById('op1');
     var AreaValue = select.options[select.selectedIndex];
-    console.log(AreaValue); 
+    // console.log(AreaValue); 
             // Area
             // var AreaValue = document.getElementById("op1");
              var area = document.getElementById("area");
@@ -730,187 +738,259 @@ function Test(x) {
 
 
 // dynamiquement
+
 function fillProductsDynamicly() {
-    // Row 2
+    // {prix: 111111, title: 'tttttttttttt', quanity: '01', totalPrix: 111111}
+       // dataProduit4PDF.shift().title : 
+       
+       // Row 2
     // Table Row 1
     var details1 = document.getElementById("details1");
     details1.innerHTML = ' &nbsp;' + 
-    (dataProduit4PDF != []) ? dataProduit4PDF.shift() : 
+    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].title : 
     "_____________"
-    // detailsIn1.value 
     + '&nbsp;';
 
-    var QTYIn1 = document.getElementById("QTYIn1");
     var qty1 = document.getElementById("qty1");
-    qty1.innerHTML = ' &nbsp;' + QTYIn1.value + '&nbsp;';
+    qty1.innerHTML = ' &nbsp;' +
+    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].quanity : 
+    "_____________"
+    + '&nbsp;';
 
-    var UnitPriceKDIn1 = document.getElementById("UnitPriceKDIn1");
+
     var UPKD1 = document.getElementById("UPKD1");
-    UPKD1.innerHTML = ' &nbsp;' + UnitPriceKDIn1.value + '&nbsp;';
+    UPKD1.innerHTML = ' &nbsp;' +  
+    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix : 
+    "_____________" 
+    + '&nbsp;';
 
-    var UnitPriceFilsIn1 = document.getElementById("UnitPriceFilsIn1");
     var UPFils1 = document.getElementById("UPFils1");
-    UPFils1.innerHTML = ' &nbsp;' + UnitPriceFilsIn1.value + '&nbsp;';
+    UPFils1.innerHTML = ' &nbsp;' +    
+     (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix*1000 : 
+    "_____________" + '&nbsp;';
 
-    var TotalAmountKDIn1 = document.getElementById("TotalAmountKDIn1");
     var TAKD1 = document.getElementById("TAKD1");
-    TAKD1.innerHTML = ' &nbsp;' + TotalAmountKDIn1.value + '&nbsp;';
+    TAKD1.innerHTML = ' &nbsp;' +  
+      (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix : 
+    "_____________" 
+    + '&nbsp;';
 
-    var TotalAmountFilsIn1 = document.getElementById("TotalAmountFilsIn1");
     var TAFils1 = document.getElementById("TAFils1");
-    TAFils1.innerHTML = ' &nbsp;' + TotalAmountFilsIn1.value + '&nbsp;';
+    TAFils1.innerHTML = ' &nbsp;' + 
+    
+    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix*1000 : 
+    "_____________"  + '&nbsp;';
+
+    if (dataProduit4PDF.length > 0) {
+        console.log("aaa");
+        dataProduit4PDF.shift();
+    } 
 
 
 
     // Row 3
     // Table Row 2
-    var detailsIn2 = document.getElementById("detailsIn2");
+
+
+
     var details2 = document.getElementById("details2");
-    details2.innerHTML = ' &nbsp;' + detailsIn2.value + '&nbsp;';
+    details2.innerHTML = ' &nbsp;' + (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].title :  "_____________"  + '&nbsp;';
 
-    var QTYIn2 = document.getElementById("QTYIn2");
     var qty2 = document.getElementById("qty2");
-    qty2.innerHTML = ' &nbsp;' + QTYIn2.value + '&nbsp;';
+    qty2.innerHTML = ' &nbsp;' +(dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].quanity :   "_____________" + '&nbsp;';
 
-    var UnitPriceKDIn2 = document.getElementById("UnitPriceKDIn2");
+
     var UPKD2 = document.getElementById("UPKD2");
-    UPKD2.innerHTML = ' &nbsp;' + UnitPriceKDIn2.value + '&nbsp;';
+    UPKD2.innerHTML = ' &nbsp;' +    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix :  "_____________"    + '&nbsp;';
 
-    var UnitPriceFilsIn2 = document.getElementById("UnitPriceFilsIn2");
     var UPFils2 = document.getElementById("UPFils2");
-    UPFils2.innerHTML = ' &nbsp;' + UnitPriceFilsIn2.value + '&nbsp;';
+    UPFils2.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].prix*1000 :  "_____________" + '&nbsp;';
 
-    var TotalAmountKDIn2 = document.getElementById("TotalAmountKDIn2");
     var TAKD2 = document.getElementById("TAKD2");
-    TAKD2.innerHTML = ' &nbsp;' + TotalAmountKDIn2.value + '&nbsp;';
+    TAKD2.innerHTML = ' &nbsp;' +     (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix :   "_____________"   + '&nbsp;';
 
-    var TotalAmountFilsIn2 = document.getElementById("TotalAmountFilsIn2");
     var TAFils2 = document.getElementById("TAFils2");
-    TAFils2.innerHTML = ' &nbsp;' + TotalAmountFilsIn2.value + '&nbsp;';
+    TAFils2.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix*1000 : "_____________"  + '&nbsp;';
 
+    if (dataProduit4PDF.length > 0) {
+        console.log("aaa");
+        dataProduit4PDF.shift();
+    }
+
+    
+
+
+    
 
     // Table Row 3
-    var detailsIn3 = document.getElementById("detailsIn3");
     var details3 = document.getElementById("details3");
-    details3.innerHTML = ' &nbsp;' + detailsIn3.value + '&nbsp;';
+    details3.innerHTML = ' &nbsp;' + (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].title :  "_____________"  + '&nbsp;';
 
-    var QTYIn3 = document.getElementById("QTYIn3");
     var qty3 = document.getElementById("qty3");
-    qty3.innerHTML = ' &nbsp;' + QTYIn3.value + '&nbsp;';
+    qty3.innerHTML = ' &nbsp;' +(dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].quanity :   "_____________" + '&nbsp;';
 
-    var UnitPriceKDIn3 = document.getElementById("UnitPriceKDIn3");
+
     var UPKD3 = document.getElementById("UPKD3");
-    UPKD3.innerHTML = ' &nbsp;' + UnitPriceKDIn3.value + '&nbsp;';
+    UPKD3.innerHTML = ' &nbsp;' +    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix :  "_____________"    + '&nbsp;';
 
-    var UnitPriceFilsIn3 = document.getElementById("UnitPriceFilsIn3");
     var UPFils3 = document.getElementById("UPFils3");
-    UPFils3.innerHTML = ' &nbsp;' + UnitPriceFilsIn3.value + '&nbsp;';
+    UPFils3.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].prix*1000 :  "_____________" + '&nbsp;';
 
-    var TotalAmountKDIn3 = document.getElementById("TotalAmountKDIn3");
     var TAKD3 = document.getElementById("TAKD3");
-    TAKD3.innerHTML = ' &nbsp;' + TotalAmountKDIn3.value + '&nbsp;';
+    TAKD3.innerHTML = ' &nbsp;' +     (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix :   "_____________"   + '&nbsp;';
 
-    var TotalAmountFilsIn3 = document.getElementById("TotalAmountFilsIn3");
     var TAFils3 = document.getElementById("TAFils3");
-    TAFils3.innerHTML = ' &nbsp;' + TotalAmountFilsIn3.value + '&nbsp;';
+    TAFils3.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix*1000 : "_____________"  + '&nbsp;';
 
-    // Table Row 4
-    var detailsIn4 = document.getElementById("detailsIn4");
+    if (dataProduit4PDF.length > 0) {
+        console.log("aaa");
+        dataProduit4PDF.shift();
+    }
+
+
+// Table Row 4
     var details4 = document.getElementById("details4");
-    details4.innerHTML = ' &nbsp;' + detailsIn4.value + '&nbsp;';
+    details4.innerHTML = ' &nbsp;' + (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].title :  "_____________"  + '&nbsp;';
 
-    var QTYIn4 = document.getElementById("QTYIn4");
     var qty4 = document.getElementById("qty4");
-    qty4.innerHTML = ' &nbsp;' + QTYIn4.value + '&nbsp;';
+    qty4.innerHTML = ' &nbsp;' +(dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].quanity :   "_____________" + '&nbsp;';
 
-    var UnitPriceKDIn4 = document.getElementById("UnitPriceKDIn4");
+
     var UPKD4 = document.getElementById("UPKD4");
-    UPKD4.innerHTML = ' &nbsp;' + UnitPriceKDIn4.value + '&nbsp;';
+    UPKD4.innerHTML = ' &nbsp;' +    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix :  "_____________"    + '&nbsp;';
 
-    var UnitPriceFilsIn4 = document.getElementById("UnitPriceFilsIn4");
     var UPFils4 = document.getElementById("UPFils4");
-    UPFils4.innerHTML = ' &nbsp;' + UnitPriceFilsIn4.value + '&nbsp;';
+    UPFils4.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].prix*1000 :  "_____________" + '&nbsp;';
 
-    var TotalAmountKDIn4 = document.getElementById("TotalAmountKDIn4");
     var TAKD4 = document.getElementById("TAKD4");
-    TAKD4.innerHTML = ' &nbsp;' + TotalAmountKDIn4.value + '&nbsp;';
+    TAKD4.innerHTML = ' &nbsp;' +     (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix :   "_____________"   + '&nbsp;';
 
-    var TotalAmountFilsIn4 = document.getElementById("TotalAmountFilsIn4");
     var TAFils4 = document.getElementById("TAFils4");
-    TAFils4.innerHTML = ' &nbsp;' + TotalAmountFilsIn4.value + '&nbsp;';
+    TAFils4.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix*1000 : "_____________"  + '&nbsp;';
+
+    if (dataProduit4PDF.length > 0) {
+        console.log("aaa");
+        dataProduit4PDF.shift();
+    }
+
 
     // Table Row 5
-    var detailsIn5 = document.getElementById("detailsIn5");
     var details5 = document.getElementById("details5");
-    details5.innerHTML = ' &nbsp;' + detailsIn5.value + '&nbsp;';
+    details5.innerHTML = ' &nbsp;' + (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].title :  "_____________"  + '&nbsp;';
 
-    var QTYIn5 = document.getElementById("QTYIn5");
     var qty5 = document.getElementById("qty5");
-    qty5.innerHTML = ' &nbsp;' + QTYIn5.value + '&nbsp;';
+    qty5.innerHTML = ' &nbsp;' +(dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].quanity :   "_____________" + '&nbsp;';
 
-    var UnitPriceKDIn5 = document.getElementById("UnitPriceKDIn5");
+
     var UPKD5 = document.getElementById("UPKD5");
-    UPKD5.innerHTML = ' &nbsp;' + UnitPriceKDIn5.value + '&nbsp;';
+    UPKD5.innerHTML = ' &nbsp;' +    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix :  "_____________"    + '&nbsp;';
 
-    var UnitPriceFilsIn5 = document.getElementById("UnitPriceFilsIn5");
     var UPFils5 = document.getElementById("UPFils5");
-    UPFils5.innerHTML = ' &nbsp;' + UnitPriceFilsIn5.value + '&nbsp;';
+    UPFils5.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].prix*1000 :  "_____________" + '&nbsp;';
 
-    var TotalAmountKDIn5 = document.getElementById("TotalAmountKDIn5");
     var TAKD5 = document.getElementById("TAKD5");
-    TAKD5.innerHTML = ' &nbsp;' + TotalAmountKDIn5.value + '&nbsp;';
+    TAKD5.innerHTML = ' &nbsp;' +     (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix :   "_____________"   + '&nbsp;';
 
-    var TotalAmountFilsIn5 = document.getElementById("TotalAmountFilsIn5");
     var TAFils5 = document.getElementById("TAFils5");
-    TAFils5.innerHTML = ' &nbsp;' + TotalAmountFilsIn5.value + '&nbsp;';
+    TAFils5.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix*1000 : "_____________"  + '&nbsp;';
+
+    if (dataProduit4PDF.length > 0) {
+        console.log("aaa");
+        dataProduit4PDF.shift();
+    }
 
     // Table Row 6
-    var detailsIn6 = document.getElementById("detailsIn6");
     var details6 = document.getElementById("details6");
-    details6.innerHTML = ' &nbsp;' + detailsIn6.value + '&nbsp;';
+    details6.innerHTML = ' &nbsp;' + (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].title :  "_____________"  + '&nbsp;';
 
-    var QTYIn6 = document.getElementById("QTYIn6");
     var qty6 = document.getElementById("qty6");
-    qty6.innerHTML = ' &nbsp;' + QTYIn6.value + '&nbsp;';
+    qty6.innerHTML = ' &nbsp;' +(dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].quanity :   "_____________" + '&nbsp;';
 
-    var UnitPriceKDIn6 = document.getElementById("UnitPriceKDIn6");
+
     var UPKD6 = document.getElementById("UPKD6");
-    UPKD6.innerHTML = ' &nbsp;' + UnitPriceKDIn6.value + '&nbsp;';
+    UPKD6.innerHTML = ' &nbsp;' +    (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].prix :  "_____________"    + '&nbsp;';
 
-    var UnitPriceFilsIn6 = document.getElementById("UnitPriceFilsIn6");
     var UPFils6 = document.getElementById("UPFils6");
-    UPFils6.innerHTML = ' &nbsp;' + UnitPriceFilsIn6.value + '&nbsp;';
+    UPFils6.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+     dataProduit4PDF[0].prix*1000 :  "_____________" + '&nbsp;';
 
-    var TotalAmountKDIn6 = document.getElementById("TotalAmountKDIn6");
     var TAKD6 = document.getElementById("TAKD6");
-    TAKD6.innerHTML = ' &nbsp;' + TotalAmountKDIn6.value + '&nbsp;';
+    TAKD6.innerHTML = ' &nbsp;' +     (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix :   "_____________"   + '&nbsp;';
 
-    var TotalAmountFilsIn6 = document.getElementById("TotalAmountFilsIn6");
     var TAFils6 = document.getElementById("TAFils6");
-    TAFils6.innerHTML = ' &nbsp;' + TotalAmountFilsIn6.value + '&nbsp;';
+    TAFils6.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+    dataProduit4PDF[0].totalPrix*1000 : "_____________"  + '&nbsp;';
+
+    if (dataProduit4PDF.length > 0) {
+        console.log("aaa");
+        dataProduit4PDF.shift();
+    }
 
     // Table Row 7
-    var detailsIn7 = document.getElementById("detailsIn7");
-    var details7 = document.getElementById("details7");
-    details7.innerHTML = ' &nbsp;' + detailsIn7.value + '&nbsp;';
+       
+       var details7 = document.getElementById("details7");
+       details7.innerHTML = ' &nbsp;' + (dataProduit4PDF.length===0) ? 
+          dataProduit4PDF[0].title :  "_____________"  + '&nbsp;';
+   
+       var qty7 = document.getElementById("qty7");
+       qty7.innerHTML = ' &nbsp;' +(dataProduit4PDF.length===0) ? 
+        dataProduit4PDF[0].quanity :   "_____________" + '&nbsp;';
+   
+   
+       var UPKD7 = document.getElementById("UPKD7");
+       UPKD7.innerHTML = ' &nbsp;' +    (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].prix :  "_____________"    + '&nbsp;';
+   
+       var UPFils7 = document.getElementById("UPFils7");
+       UPFils7.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+        dataProduit4PDF[0].prix*1000 :  "_____________" + '&nbsp;';
+   
+       var TAKD7 = document.getElementById("TAKD7");
+       TAKD7.innerHTML = ' &nbsp;' +     (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].totalPrix :   "_____________"   + '&nbsp;';
+   
+       var TAFils7 = document.getElementById("TAFils7");
+       TAFils7.innerHTML = ' &nbsp;' +  (dataProduit4PDF.length===0) ? 
+       dataProduit4PDF[0].totalPrix*1000 : "_____________"  + '&nbsp;';
+   
+       if (dataProduit4PDF.length > 0) {
+           console.log("aaa");
+           dataProduit4PDF.shift();
+       }
 
-    var QTYIn7 = document.getElementById("QTYIn7");
-    var qty7 = document.getElementById("qty7");
-    qty7.innerHTML = ' &nbsp;' + QTYIn7.value + '&nbsp;';
 
-    var UnitPriceKDIn7 = document.getElementById("UnitPriceKDIn7");
-    var UPKD7 = document.getElementById("UPKD7");
-    UPKD7.innerHTML = ' &nbsp;' + UnitPriceKDIn7.value + '&nbsp;';
-
-    var UnitPriceFilsIn7 = document.getElementById("UnitPriceFilsIn7");
-    var UPFils7 = document.getElementById("UPFils7");
-    UPFils7.innerHTML = ' &nbsp;' + UnitPriceFilsIn7.value + '&nbsp;';
-
-    var TotalAmountKDIn7 = document.getElementById("TotalAmountKDIn7");
-    var TAKD7 = document.getElementById("TAKD7");
-    TAKD7.innerHTML = ' &nbsp;' + TotalAmountKDIn7.value + '&nbsp;';
-
-    var TotalAmountFilsIn7 = document.getElementById("TotalAmountFilsIn7");
-    var TAFils7 = document.getElementById("TAFils7");
-    TAFils7.innerHTML = ' &nbsp;' + TotalAmountFilsIn7.value + '&nbsp;';
 }
